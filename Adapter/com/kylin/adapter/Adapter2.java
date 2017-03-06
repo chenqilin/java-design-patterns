7d8778881ce188f2177bb77da38d1aa01c39dd5a
chenqilin <chenqilin@1022>
2017-03-06 23:01:48 +0800

    适配器模式

---
+++ Adapter/com/kylin/adapter/Adapter2.java
@@ -0,0 +1,16 @@
+package com.kylin.adapter;
+
+public class Adapter2 implements Target{
+	// 直接关联被适配类  
+    private Adaptee adaptee;  
+      
+    // 可以通过构造函数传入具体需要适配的被适配类对象  
+    public Adapter2 (Adaptee adaptee) {  
+        this.adaptee = adaptee;  
+    }  
+      
+    public void request() {  
+        // 这里是使用委托的方式完成特殊功能  
+        this.adaptee.specificRequest();  
+    }  
+}
